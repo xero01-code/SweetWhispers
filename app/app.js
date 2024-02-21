cf9badf69e733bb7c77d7f0f4a0c459400216a29
xero01-code <legan.01apolinar@gmail.com>
2024-02-20 20:50:11 -0700

    SwieetWhispers

---
+++ app/app.js
@@ -0,0 +1,79 @@
+const jsConfetti = new JSConfetti()
+let botonReiniciar=document.querySelector('.botonReinicio')
+let botonNo = document.querySelector(".botonNo");
+let textoCambia = document.querySelector(".textoCambio");
+let botonsi=document.querySelector('.botonSi');
+let imagenLove = document.querySelector(".imagen");
+
+botonNo.addEventListener("click", respuestaNo);
+botonsi.addEventListener('click',respuestaSi);
+
+
+function respuestaNo() {
+  let frases = [
+    "Would you like to be my Valentine?",
+    "Would you like to be my partner?",
+    "Do you want to be my official partner?",
+    "Do you want to be my boyfriend/girlfriend?",
+    "Do you want to be a special part of my life?",
+    "Would you like to be my special person?",
+    "Do you want to be my other half?",
+    "Would you like to be my love?",
+    "Would you like to be my better half?",
+    "Do you want to be my soulmate?",
+    "Would you like to be my sweetheart?",
+    "Would you like to be my Valentine's Day date?",
+    "Would you like to be my life partner?",
+    "Would you like to be my dear?",
+    "Do you want to be my confidant and my love?",
+    "Would you like to be the reason for my smiles?",
+    "Do you want to be the person I share all my days with?",
+    "Do you want to be my reason to believe in love?",
+  ];
+  let imagenes = [
+    "../img/uno.gif",
+    "../img/dos.gif",
+    "../img/tres.gif",
+    "../img/cuatro.gif",
+    "../img/cinco.gif",
+    "../img/siete.gif",
+    "../img/ocho.gif",
+  ];
+  let valorRandom = frases[Math.floor(Math.random() * frases.length)];
+  let RandomImagen = imagenes[Math.floor(Math.random() * imagenes.length)];
+
+  textoCambia.innerText = valorRandom;
+  imagenLove.src = RandomImagen;
+}
+
+let botonReinicioCreado=false;
+
+function respuestaSi(){
+
+    jsConfetti.addConfetti()
+    imagenLove.src='../img/lovely-cats.gif'
+    textoCambia.innerText='I would know you felt the same way I do, I love you. ';
+    botonNo.classList.add('bloqueo')
+
+    if(!botonReinicioCreado){
+      creacionNuevoBoton()
+      botonReinicioCreado=true;
+    }
+}
+
+function creacionNuevoBoton(){
+
+    let boton = document.createElement('button')
+    boton.innerHTML='Reset'
+    boton.setAttribute('class','botonReinicio')
+    let contenedorBoton=document.querySelector('.container__boton')
+    contenedorBoton.appendChild(boton)
+    boton.addEventListener('click',reiniciarPrograma)
+}
+
+
+function reiniciarPrograma(){
+    window.location.reload()
+}
+
+
---
+++ readme.md
@@ -0,0 +1,22 @@
+
+# Proyecto de san valentin
+
+Pregunta de Amor es un juego encantador diseñado para ayudarte a expresar tus sentimientos a esa persona especial,¿Estás listo para dar el siguiente paso en tu relación y preguntarle si quiere ser tu enamorada o enamorado?.
+
+<img class="imagen" src="img/imgReadme/FondoPrincipal.png" alt="">
+
+
+# caracteristicas principales
+
+1.- Cada vez que tu persona amada haga click en el boton de no, saldra una gif aleatoria y abajo con una frase preguntando de que si quieres ser su amada/o.
+
+2.- Cuando le hagas click en el boton de si, saldra una imagen diferente pero con una gran sorpresa.
+
+# Imagenes cuando diga que no
+
+<img class="imagen" src="img/imgReadme/Uno.png" alt="">
+<img class="imagen" src="img/imgReadme/dos.png" alt="">
+
+# Imagen cuando diga que si
+
+<img class="imagen" src="img/imgReadme/Tres.png" alt="">
